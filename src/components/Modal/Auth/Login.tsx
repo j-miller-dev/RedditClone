ffece34c1324e4c33f823ccc02b2ef7ed8ea7943
Jason Miller <jasonmiller.dev@gmail.com>
2023-02-18 21:16:28 +1100

    first commit

---
+++ src/components/Modal/Auth/Login.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { Input } from "@chakra-ui/react";
+
+type LoginProps = {};
+
+const Login: React.FC<LoginProps> = () => {
+  const { loginForm, setLoginForm } = useState({
+    email: "",
+    password: "",
+  });
+
+  return (
+    <form>
+      <Input />
+      <Input />
+    </form>
+  );
+};
+export default Login;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Flex, Image } from "@chakra-ui/react";
+import React from "react";
+import RightContent from "./RightContent/RightContent";
+import SearchInput from "./SearchInput";
+
+const Navbar: React.FC = () => {
+  return (
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" height="30px" alt="" />
+        <Image
+          src="/images/redditText.svg"
+          height="46px"
+          display={{ base: "none", md: "unset" }}
+          alt=""
+        />
+      </Flex>
+      <SearchInput />
+      <RightContent />
+      {/* /* <Directory /> */}
+    </Flex>
+  );
+};
+
+export default Navbar;
