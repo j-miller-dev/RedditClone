6694a327a9e648f8a9ee8c22d5bf6abaeffd8348
Jason Miller <jasonmiller.dev@gmail.com>
2023-03-13 22:31:58 +1100

    Issue solved: Simply wasn't adding r/ for the server side rendering [communityId] folder

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,16 +1,28 @@
-import React from "react";
+import { useEffect } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { GetServerSidePropsContext } from "next";
 import { Community } from "@/src/atoms/communitiesAtom";
-import { firestore } from "@/src/firebase/clientApp";
+import { auth, firestore } from "../../../firebase/clientApp";
 import safeJsonStringify from "safe-json-stringify";
+import NotFound from "@/src/components/Community/NotFound";
+import Header from "@/src/components/Community/Header";
 
 type CommunityPageProps = {
   communityData: Community;
 };
 
 const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
-  return <div>WELCOME TO {communityData.id}</div>;
+  console.log("here is data", communityData);
+
+  if (!communityData) {
+    return <NotFound />;
+  }
+
+  return (
+    <>
+      <Header communityData={communityData} />
+    </>
+  );
 };
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
@@ -25,9 +37,11 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
     return {
       props: {
-        communityData: JSON.parse(
-          safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
-        ),
+        communityData: communityDoc.exists()
+          ? JSON.parse(
+              safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+            )
+          : "",
       },
     };
   } catch (error) {
