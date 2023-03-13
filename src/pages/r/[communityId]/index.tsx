2d6b1ce36a8f25b6ddf3259cc99733c6953094c2
Jason Miller <jasonmiller.dev@gmail.com>
2023-03-13 18:20:05 +1100

    Updates to communities, ISSUE: 404 error when trying to access an existing community url

---
+++ src/pages/r/[communityId]/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { GetServerSidePropsContext } from "next";
+import { Community } from "@/src/atoms/communitiesAtom";
+import { firestore } from "@/src/firebase/clientApp";
+import safeJsonStringify from "safe-json-stringify";
+
+type CommunityPageProps = {
+  communityData: Community;
+};
+
+const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+  return <div>WELCOME TO {communityData.id}</div>;
+};
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  // get community data and pass it to client
+  try {
+    const communityDocRef = doc(
+      firestore,
+      "communities",
+      context.query.communityId as string
+    );
+    const communityDoc = await getDoc(communityDocRef);
+
+    return {
+      props: {
+        communityData: JSON.parse(
+          safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+        ),
+      },
+    };
+  } catch (error) {
+    // Could add error page here
+    console.log("getServerSideProps error", error);
+  }
+}
+
+export default CommunityPage;
