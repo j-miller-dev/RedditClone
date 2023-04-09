ca6ca3dacb76c2f796c6f2d3e617e233d5dec0d9
Jason Miller <jasonmiller.dev@gmail.com>
2023-04-10 00:28:37 +1000

    last bug fix, issue with children props

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 
-const Layout: React.FC = ({ children }) => {
+// https://stackoverflow.com/questions/59106742/typescript-error-property-children-does-not-exist-on-type-reactnode
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<Props> = ({ children }) => {
   return (
     <>
       <Navbar />
