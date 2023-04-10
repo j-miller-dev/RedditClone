e187996f2d6fc31700383d08ad5349486a07a16b
Jason Miller <jasonmiller.dev@gmail.com>
2023-04-10 11:47:35 +1000

    Fixed textInputProps types

--- src/components/Posts/PostForm/TextInputs.tsx
+++ src/components/Posts/PostForm/TextInputs.tsx
@@ -1,7 +1,17 @@
 import { Button, Flex, Input, Stack, Textarea } from "@chakra-ui/react";
 import React from "react";
 
-type TextInputsProps = {};
+type TextInputsProps = {
+  textInputs: {
+    title: string;
+    body: string;
+  };
+  onChange: (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  handleCreatePost: () => void;
+  loading: boolean;
+};
 
 const TextInputs: React.FC<TextInputsProps> = ({
   textInputs,
